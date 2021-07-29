3bb441d0119c3aa6fc58b86553569b8e935783e2
Alexander Zamyhlyaev <alexzzam@gmail.com>
2021-07-29 13:11:05 +0300

    Add Mongo support

---
+++ src/main/kotlin/com/github/alexzam/home/retirementplanner/StorageService.kt
@@ -0,0 +1,10 @@
+package com.github.alexzam.home.retirementplanner
+
+import com.github.alexzam.home.retirementplanner.model.World
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+class StorageService {
+    val db = KMongo.createClient().coroutine.getDatabase("planner")
+    val worlds = db.getCollection<World>()
+}
